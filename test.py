52654d90837a659d74e077c99d0a5dfcd231931c
Javier Pérez <javier.perez@zippedi.com>
2022-12-07 21:53:23 -0300

    pytest

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-# Using Chrome to access web
-driver = webdriver.Chrome()
-# Open the website
-driver.get('http://44.211.209.215:3000/')
+import pytest
+
+@pytest.fixture()
+def testSetup():
+    global driver
+    # Using Chrome to access web
+    driver = webdriver.Chrome()
+    # Open the website
+    yield
+    driver.quit()
+    
+
+def testGet(testSetup):
+    driver.get('http://44.211.209.215:3000/')
+    element = driver.find_element(By.CLASS_NAME, "Tarjeta").text
+    assert "Prueba de carne" in element
