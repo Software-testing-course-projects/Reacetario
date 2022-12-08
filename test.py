71540a06596b16004e5829d8be6ab7a277b6bc0c
Javier Pérez <javier.perez@zippedi.com>
2022-12-07 21:27:34 -0300

    added required files

---
+++ test.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+# Using Chrome to access web
+driver = webdriver.Chrome()
+# Open the website
+driver.get('http://44.211.209.215:3000/')
